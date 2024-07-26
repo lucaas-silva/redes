acb3314de1134af97b387b3fb6bd4f1bc4c679ae
Lucas <lucaseduardo.s07@gmail.com>
2024-07-26 11:46:30 -0300

    criando parte 2 do trabalho

---
+++ server_working_directory/tcp_server.py
@@ -0,0 +1,122 @@
+import logging
+import socket
+import os
+import sys
+import hashlib
+import base64
+import json
+import subprocess
+
+buf_size = 4096 
+file_path = os.path.abspath(sys.argv[0])
+server_path = os.path.dirname(file_path)
+
+def send_msg(sock, operation, result=None, file_name=None, status=None, file=None, hash=None, error_msg=None):
+    match operation:
+        case "ls":
+            msg = {"operation": "ls", "ls": result}
+            msg = json.dumps(msg)
+            sock.send(bytes(msg, "utf-8"))
+        case "put":
+            msg = {
+                    "operation": "put",
+                    "file": file_name,
+                    "status": status,
+                    }
+            msg = json.dumps(msg)
+            sock.send(bytes(msg, "utf-8"))
+        case "get":
+            msg = {
+                    "operation": "get",
+                    "file": (file_name, file),
+                    "hash": hash,
+                    }
+            msg = json.dumps(msg)
+            sock.send(bytes(msg, "utf-8"))
+        case "error":
+            msg = {"operation": "error", "error": error_msg} 
+            msg = json.dumps(msg)
+            sock.send(bytes(msg, "utf-8"))
+
+
+def ls(sock):
+    if os.name == 'nt':
+        command = ['dir']
+    else:
+        command = ['ls']
+
+    result = subprocess.run(command, capture_output=True, text=True, shell=True).stdout
+    send_msg(sock=sock, operation='ls', result=result)
+
+def put(client_sock, file, file_name, hash):
+    file = base64.b64decode(file)
+    hash_server = hashlib.md5(file).hexdigest()
+    
+    if hash_server == hash:
+        with open(file_name, "wb") as f:
+            f.write(file)
+        logging.info(f"File {file_name} saved successfully.")
+        send_msg(sock = client_sock, operation = "put", file_name = file_name, status = True)
+    else:
+        send_msg(sock = client_sock, operation = "put", file_name = file_name, status = False)
+        logging.info("ERROR...")
+
+def get(client_sock, file_name):
+    try:
+        with open(file_name, "rb") as f:
+            file_data = f.read()
+
+        hash = hashlib.md5(file_data).hexdigest()
+        file_data64 = base64.b64encode(file_data).decode("utf-8")
+        send_msg(sock=client_sock, operation="get", file_name=file_name, file=file_data64, hash=hash)
+    except Exception:
+        send_msg(sock=client_sock, operation="error", error_msg = f"File {file_name} does not exits")
+
+def read_msg(client_sock, msg):
+    match msg['command']:
+        case "ls":
+            ls(client_sock) 
+        case "put":
+            put(client_sock, msg['file'][1], msg['file'][0], msg['hash']) 
+        case "get":
+            get(client_sock, msg['file']) 
+
+def main(localIp, port):
+    logging.info("Starting TCP server at port %d...", port)
+    srv_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    srv_sock.bind((localIp, port))
+    srv_sock.listen(1)
+
+    while True:
+        client_sock, client_addr = srv_sock.accept()
+        logging.info("Client connect from %s", client_addr)
+        data = ""
+        while True:
+            part = client_sock.recv(buf_size).decode("utf-8")
+            if not part:
+                break
+            data += part
+
+            try:
+                msg = json.loads(data)
+                read_msg(client_sock, msg)
+                logging.info("Message receveid: %s", msg)
+                data = ""
+            except json.JSONDecodeError:
+                continue
+            # msg = json.loads(bytes.decode(client_sock.recv(buf_size), "utf-8"))
+            # read_msg(msg) 
+
+if __name__ == "__main__":
+    format = "[%(asctime)s.%(msecs)03d %(message)s]"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Starting TPC server...")
+
+    while True:
+        srv_port = int(input("\tType the server port (1 - 65535): "))
+        if srv_port >= 1 and srv_port <= 65535:
+            break
+
+    srv_addr = "127.0.0.1"
+
+    main(srv_addr, srv_port)
---
+++ tcp_client.py
@@ -0,0 +1,91 @@
+import socket
+import ipaddress 
+import logging
+import hashlib
+import json
+import base64
+
+def get(file_name, file, hash):
+    file = base64.b64decode(file)
+    hash_server = hashlib.md5(file).hexdigest()
+    
+    if hash_server == hash:
+        with open(file_name, "wb") as f:
+            f.write(file)
+        logging.info(f"File {file_name} saved successfully.")
+    else:
+        logging.info("Error transferring file, request again")
+
+def put(file_name):
+    try:
+        with open(file_name, "rb") as f:
+            file_data = f.read()
+
+        hash = hashlib.md5(file_data).hexdigest()
+        file_data64 = base64.b64encode(file_data).decode("utf-8")
+        print("abre")
+        return {"command":"put", "file":(file_name, file_data64), "hash":hash}
+    except FileNotFoundError:
+        logging.info(f"File {file_name} does not exits")
+
+def read_message(msg):
+    msg = json.loads(msg) 
+
+    match msg['operation']:
+        case "put":
+            if msg['status']:
+                logging.info("File %s uploaded successfully", msg['file'])
+            else:
+                logging.info("Error sending %s", msg['file'])
+        case "ls":
+            logging.info(f"\n{msg['ls']}")
+        case "get":
+            get(msg['file'][0], msg['file'][1], msg['hash'])
+        case "error":
+            logging.info(msg['error'])
+        
+
+def generate_message():
+    command = input("Type a command: ").split()
+
+    match command[0]:
+        case "ls":
+            return {"command": "ls"} 
+        case "put":
+            return put(command[1])
+        case "get":
+            return {"command": "get", "file": command[1]}
+
+def main(srv_addr, srv_port):
+    logging.info("Connecting TPC client to %s:%d...", srv_addr, srv_port)
+    client_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    client_sock.connect((srv_addr, srv_port))
+    while True:
+        msg = generate_message()
+        if msg:
+            msg = json.dumps(msg)
+            client_sock.send(bytes(msg, "utf-8"))
+            logging.info("Message sent")
+
+            msg_recv = client_sock.recv(4096).decode("utf-8")
+            read_message(msg_recv)
+
+if __name__ == "__main__":
+    format = "[%(asctime)s.%(msecs)03d] %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Starting TCP client...")
+
+    while True:
+        srv_addr = input("\tType the server IP address: ")
+        try:
+            ip_obj = ipaddress.ip_address(srv_addr)
+            break
+        except ValueError:
+            logging.info("IP address invalid")
+
+    while True:
+        srv_port = int(input("\tType the server port (1 - 65535): "))
+        if (srv_port >= 1 and srv_port <= 65535):
+            break
+
+    main(srv_addr, srv_port)
